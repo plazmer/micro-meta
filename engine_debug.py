2b3f7f30aaaf485b07a72bece203bdbbbd5d9408
AVP <praw2003@gmail.com>
2017-08-22 16:02:30 +0500

    new plugin dmir

--- engine_debug.py
+++ engine_debug.py
@@ -2,9 +2,9 @@ import os
 import sys
 import plugin
 import utils
-from plugins.avito import request, parse
+from plugins.dmir import request, parse
 
-query = "труба нержавеющая"
+query = "компрессор"
 
 params = plugin.default_params()
 req = request(query, params, None)
@@ -12,4 +12,4 @@ print(req)
 
 html = utils.http(req)
 for r in parse(html):
-	print(r['photo'])
+	print(r)
